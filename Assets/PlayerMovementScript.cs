4728ed9c8730cb44cf7fafecdb1171a9b9199a6f
krb365 <105557487+krb365@users.noreply.github.com>
2022-05-23 16:04:30 -0400

    Keaton partial Update
    Addition of Player and most assets into the play space. Player movement and Camera tracking is complete. Known issues include: cutting player model out of the rectangle, player sprite appears under the play space, camera does not include visibility reduciton

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -16,8 +16,8 @@ public class PlayerMovementScript : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
