2520a989a1ca903d0fb9c1c7bb0777b827272899
LucaRug <ltr37@drexel.edu>
2022-05-24 19:22:25 -0400

    Start/Options
    Start/Options menu implemented

---
+++ Assets/menuAssets/menuScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
